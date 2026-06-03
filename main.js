8f9f735d762ca0d22e5e6d9b9510cc818677e6dc
RikJongejan <6044223@mborijnland.nl>
2026-06-03 11:34:43 +0200

    Main.js aangepast

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
-const canvas = document.getElementById('PixelCanvas');
-const ctx = canvas.getContext('2d');
-
-const grid = new Grid(16, 16, 32);
-grid.render(ctx);
-
-const pen = new Pen();
-const gum = new Gum();
-
-console.log(pen.name);
-console.log(gum.name);
+const app = new App(16, 16, 32);
+app.init();
